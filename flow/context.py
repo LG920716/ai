28b3f166a8ea92d8b623557ff145ecd0bde0647f
ray.huang <ray.huang@rayhuangdeMacBook-Pro.local>
2024-09-11 16:13:06 +0800

    edit the url to new backend

--- flow/context.py
+++ flow/context.py
@@ -4,13 +4,13 @@ import json
 
 @tool
 def call_api(session_id):
-    url = "https://localhost:7146/api/Context"
+    url = "http://140.136.202.125/api/Context"
 
     params = {
         'sessionId': session_id
     }
     
-    response = requests.get(url, params=params, verify=False)  # verify=False 是為了跳過 SSL 驗證，僅在開發時使用
+    response = requests.get(url, params=params, verify=False)  # verify=False 是為了跳過 SSL 驗證
 
     response_json = response.json()
     results = []

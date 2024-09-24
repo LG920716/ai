22fc5557713e961c58ea0c832a8d38572e185713
ray.huang <ray.huang@rayhuangdeMacBook-Pro.local>
2024-09-24 13:59:07 +0800

    first

---
+++ flow copy/context.py
@@ -0,0 +1,22 @@
+import requests
+from promptflow.core import tool
+import json
+
+@tool
+def call_api(session_id: str) -> str:
+    url = "http://140.136.202.125/api/Context"
+
+    params = {
+        'sessionId': session_id
+    }
+    
+    response = requests.get(url, params=params, verify=False)  # verify=False 是為了跳過 SSL 驗證
+
+    response_json = response.json()
+    results = []
+    for item in response_json:
+        json_data = {
+            "questionQuestion": item.get("questionQuestion")
+        }
+        results.append(json.dumps(json_data, ensure_ascii=False))
+    return "\n".join(results)
